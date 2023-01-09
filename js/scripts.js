5ff55d5dbbb271ff812992284d17379dca4a9c0c
JesusCasablanca-examen <jcasablancai2223@politecnics.barcelona>
2023-01-09 09:58:13 +0100

    actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,46 @@
 
 
 function hanFetClick(){
- alert("Has apretado el boton:"+ event.currentTarget);
-
-
-
+ alert("Has apretado:"+ event.currentTarget);
 
 }
+
+function reaccionaEsdeveniment(){
+    alert("Esta tarea es:"+ event.currentTarget);
+
+}
+
+function espera(){
+    alert("Empieza el evento");
+    setInterval(200000000);
+    alert("Acaba el evento");
+}
+
+/*function novaTasca(){
+    tasca = document.getElementById("tasca").value;
+
+    data = document.querySelector(".lista");
+    document.body.appendChild(tasca);
+
+
+    
+}*/
+
+
+
+function novaTasca() {
+    // create a new div element
+    const nouElement = document.createElement("li");
+
+    tasca = document.getElementById("tasca").value;
+    // and give it some content
+    const novaTasca = document.createTextNode(tasca);
+  
+    // add the text node to the newly created div
+    nouElement.appendChild(novaTasca);
+  
+    // add the newly created element and its content into the DOM
+    const llistaTasques = document.getElementById("lista");
+    document.body.insertBefore(nouElement, llistaTasques);
+  }
+  
