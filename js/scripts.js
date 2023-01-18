855ca44f583e19e5e805984c551fd023a5dce7a4
JesusCasablanca-examen <jcasablancai2223@politecnics.barcelona>
2023-01-18 10:38:36 +0100

    updateado

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,37 @@ function reaccionaEsdeveniment(){
 
 }
 
-function espera(){
-    alert("Empieza el evento");
-    setInterval(200000000);
-    alert("Acaba el evento");
-}
+
 
 
 
 function novaTasca() {
-    // create a new div element
+    
     const nouElement = document.createElement("li");
 
     tasca = document.getElementById("tasca").value;
-    // and give it some content
+    
     const novaTasca = document.createTextNode(tasca);
   
-    // add the text node to the newly created div
+    
     nouElement.appendChild(novaTasca);
   
-    // add the newly created element and its content into the DOM
+   
     const llistaTasques = document.getElementById("lista");
     document.body.insertBefore(nouElement, llistaTasques);
+
+  }
+
+  function espera(){
+    
+    setTimeout(numeroSuerte,2000);
+    
+}
+
+  function numeroSuerte(){
+    
+
+    numero = Math.floor(Math.random() * 10);
+    alert("Tu número de la suerte es el: "+ numero);
   }
   
